204fce36d9d60ed78ef9b1473cb8220c742908ea
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2018-07-03 16:39:21 +0900

    VRAMの型の誤りを修正

--- software/vga/vga.c
+++ software/vga/vga.c
@@ -2,19 +2,34 @@
 
 #include "platform.h"
 
-#define VRAM ((volatile unsigned int *) VGA_MMAP_ADDR)
+#define VRAM ((volatile unsigned char *) VGA_MMAP_ADDR)
+#define SCREEN_WIDTH  640
+#define SCREEN_HEIGHT 480
+
+void drawRect(int x, int y, int width, int height, char color)
+{
+  int px, py;
+
+  for (px = x; px < x + width; px++) {
+    VRAM[ y               * SCREEN_WIDTH + px] = color;
+    VRAM[(y + height - 1) * SCREEN_WIDTH + px] = color;
+  }
+
+  for (py = y; py < y + height; py++) {
+    VRAM[py * SCREEN_WIDTH + x            ] = color;
+    VRAM[py * SCREEN_WIDTH + x + width - 1] = color;
+  }
+}
+
 
 int main()
 {
-  int hexspeak = 0xACCE55ED;
-  int n;
-
   puts("VGA VRAM!\n");
 
-  VRAM[0] = hexspeak;
-  n = VRAM[0];
-
-  SEG7_REG(SEG7_VAL) = n;
+  drawRect(  0,   0, 640, 480, 0xFF);
+  drawRect(100,  20, 320, 240, 0xE0);
+  drawRect(200,  70, 100, 100, 0x1C);
+  drawRect(360, 140, 200, 150, 0x03);
 
   return 0;
 }
